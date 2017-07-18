032ae8b58105799bdf19bea9e6a101b8fa52ead9
jan K. Pluta <jkpluta@gmail.com>
2017-07-18 13:52:24 +0200

    Ajax

---
+++ js/script.js
@@ -0,0 +1,21 @@
+runAjax = function(sel, href) {
+  $(sel).html('<img src="./img/spinner.gif">')
+  $.ajax({
+    url: href,
+    cache: false,
+    success: function(html) {
+      updateAjax(sel, html)
+    },
+    error: function(xhr, status, error) {
+      $(sel).html('<img src="./img/error.png"> <b>' + status + '</b> <i>' + error + "</i>")
+    }
+  })
+}
+
+updateAjax = function(sel, html) {
+  $(sel).html(html);
+}
+
+$(document).ready(function() {
+  runAjax('#bk1', './bookmarks.html')
+})
