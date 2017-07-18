9375c154297c95b499a73688557f216e26e7404e
jan K. Pluta <jkpluta@gmail.com>
2017-07-18 20:30:58 +0200

    Zakładki

--- js/script.js
+++ js/script.js
@@ -14,8 +14,30 @@ runAjax = function(sel, href) {
 
 updateAjax = function(sel, html) {
   $('dl', html).each(function(idx) {
-    $('#bk' + idx % 4).append($('dl', html).eq(idx).prev())
-    $('#bk' + idx % 4).append($('dl', html).eq(idx))
+    //$('#bk' + idx % 4).append($('dl', html).eq(idx).prev().prop("tagName"))
+    $('#bk' + idx % 4).append('<h4>' + $('dl', html).eq(idx).prev().html() + '</h4>')
+    //$('#bk' + idx % 4).append($('dl', html).eq(idx).prev())
+    //$('#bk' + idx % 4).append($('dl', html).eq(idx))
+
+    var dl = $('dl', html).eq(idx).children('dt')
+
+    $('#bk' + idx % 4).append('<p><dl>')
+
+    /*
+    $('a', dl).each(function(jdx) {
+      $('#bk' + idx % 4).append('<dt>' + $('a', dl).eq(jdx)[0].outerHTML + '</dt>')
+    })
+    */
+
+    dl.each(function(jdx, dt) {
+      dl.eq(jdx).children('a').each(function(kdx) {
+        a = dl.eq(jdx).children('a').eq(kdx)
+        $('#bk' + idx % 4).append('<dt>' + a[0].outerHTML + '</dt>')
+      })
+    })
+
+    $('#bk' + idx % 4).append('</dl></p>')
+
   })
 }
 
