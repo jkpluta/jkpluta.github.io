2c401405b35e76a781cd7a15fa8e3fc6ad8a5161
jan K. Pluta <jkpluta@gmail.com>
2017-07-26 12:07:07 +0200

    Ikony

--- js/script.js
+++ js/script.js
@@ -43,32 +43,11 @@ function updateBookmarks(sel, base, html) {
 
 function updateIcons(sel, base, html) {
   $(sel).html('')
-  /*
-  var listy = $('dl', html)
-  for (var i = 0; i < listy.length; i++) {
-    $(sel).append('<div id="bk' + i + '" class="col-sm-6 col-md-4 col-lg-3"></div>')
-
-    var bieżąca_lista = listy.eq(i)
-    var bieżące_pozycje = bieżąca_lista.children('dt')
-
-    $('#bk' + i).append('<h4>' + bieżąca_lista.prev().html() + '</h4>')
-    $('#bk' + i).append('<p><dl>')
-
-    for (var j = 0; j < bieżące_pozycje.length; j++) {
-
-      var bieżąca_pozycja = bieżące_pozycje.eq(j)
-      var bieżące_linki = bieżąca_pozycja.children('a')
-      
-      if (bieżące_linki.length > 0) {
-        var bieżący_link = bieżące_linki.first()
-        $('#bk' + i).append('<dt>' + bieżący_link[0].outerHTML + '</dt>')
-      }
-
-    }
-
-    $('#bk' + i).append('</dl></p>')
+  var links = $('a[icon]', html)
+  for (var i = 0; i < links.length; i++) {
+    var link = links.eq(i)
+    $(sel).append('<a href="' + link.attr('href') + '"><img src="' + link.attr('ICON') + '" alt="' + link.text() + '" title="' + link.text() + '"></a> ')
   }
-  */
 }
 
 function updateInfo(sel, base, html) {
