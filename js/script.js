edbf13bc2a139a047bb0027a72bb6865739f3cc1
jan K. Pluta <jkpluta@gmail.com>
2017-07-26 11:47:21 +0200

    Ikony

--- js/script.js
+++ js/script.js
@@ -41,11 +41,42 @@ function updateBookmarks(sel, base, html) {
   }
 }
 
+function updateIcons(sel, base, html) {
+  $(sel).html('')
+  /*
+  var listy = $('dl', html)
+  for (var i = 0; i < listy.length; i++) {
+    $(sel).append('<div id="bk' + i + '" class="col-sm-6 col-md-4 col-lg-3"></div>')
+
+    var bieżąca_lista = listy.eq(i)
+    var bieżące_pozycje = bieżąca_lista.children('dt')
+
+    $('#bk' + i).append('<h4>' + bieżąca_lista.prev().html() + '</h4>')
+    $('#bk' + i).append('<p><dl>')
+
+    for (var j = 0; j < bieżące_pozycje.length; j++) {
+
+      var bieżąca_pozycja = bieżące_pozycje.eq(j)
+      var bieżące_linki = bieżąca_pozycja.children('a')
+      
+      if (bieżące_linki.length > 0) {
+        var bieżący_link = bieżące_linki.first()
+        $('#bk' + i).append('<dt>' + bieżący_link[0].outerHTML + '</dt>')
+      }
+
+    }
+
+    $('#bk' + i).append('</dl></p>')
+  }
+  */
+}
+
 function updateInfo(sel, base, html) {
   $('#info').html(html)
 }
 
 $(document).ready(function() {
-  startAjax('#bks', '#bke', 'https://jkpluta.github.io', '/bookmarks.html', updateBookmarks)
   startAjax('#info', '#info', 'https://jkpluta.github.io', '/info.html', updateInfo)
+  startAjax('#icns', '#icns', 'https://jkpluta.github.io', '/icons.html', updateIcons)
+  startAjax('#bks', '#bke', 'https://jkpluta.github.io', '/bookmarks.html', updateBookmarks)
 })
