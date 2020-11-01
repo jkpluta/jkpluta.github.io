ccc90121e07577ee233bc6317d510650d3d1d33c
Jan K. Pluta <jkpluta@gmail.com>
2020-11-01 12:17:33 +0100

    PWA

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
+self.addEventListener('install', event => {
+    console.log('Install...');
+    event.waitUntil(
+        caches.open('jkpluta').then(function(cache) {
+            return cache.addAll(['index.html', 'info.html', 'bookmarks.html', 'icons.html', 'manifest.json', 'service-worker.js', 'js/script.js']);
+        })
+    );
+});
+
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.open('jkpluta').then(function(cache) {
