54186e31a5129f7faf1032a3cafe4ee08a5f4082
jan K. Pluta <jkpluta@gmail.com>
2025-05-09 10:02:24 +0200

    Porządki

--- js/script.js
+++ js/script.js
@@ -14,9 +14,6 @@ function start(sel, spnr, href, func) {
       $(spnr).html('<img src="../img/error.png"> <b>' + error + '</b>');
   });
 }
-function updateMainInfo(sel, html) {
-  $('#info').html(html);
-}
 function updateMainBookmarks(sel, html) {
   $(sel).html('');
   var listy = $('dl', html);
@@ -66,146 +63,6 @@ function startJson(sel, spnr, href, func) {
         if (spnr != null)
             $(spnr).html('<img src="../img/error.png"> <b>' + error + '</b>');
     });
-}
-function updateMainGists(sel, data) {
-    var gists = data;
-    var token = localStorage.getItem('token');
-    if(token) {
-        $(sel).prev().append('<div class="col-12"><h4>Zapiski <i id="add-gist" class="fa fa-plus"></i></h4></div>');
-        $('#add-gist').click(function() {
-            $('#gist-id').val('');
-            $('#title').val('')
-            $('#url').val('')
-            $('#description').val('')
-        $('.modal-title').text('Nowy zapisek')
-            $('#modal').modal();
-        })
-    } else {
-        if (gists.length == 0)
-            $(sel).prev().hide();
-        else
-            $(sel).prev().append('<div class="col-12"><h4>Zapiski</h4></div>');
-    }
-    for (var idx in gists) {
-        var gist = gists[idx];
-        if (gist.description === 'Jan K. Pluta')
-            startJson(gist, null, gist.files['bookmark.json'].raw_url, updateMainGist);
-    }
-}
-function updateMainGist(gist, data) {
-    if (data.type === "jkpluta.bookmark") {
-        var item = $('<div class="col-sm-12 col-md-6 col-lg-4"></div>');
-        $('#gists').append(item)
-        if (data.url !== '') {
-            var link = $('<a target="_blank"></a>');
-            item.append(link);
-            link.attr('href', data.url);
-            link.text(data.title);
-        } else {
-            item.append('<b>' + data.title + '</b>')            
-        }
-        var token = localStorage.getItem('token');
-        if (token) {
-            $('#mnu').css('display', 'inline')
-            item.append(' <i id="edit-' + gist.id + '" class="fa fa-edit"></i>')
-            item.append(' <i id="del-' + gist.id + '" class="fa fa-times"></i>')
-            $('#edit-' + gist.id).click(function() {
-                $('#gist-id').val(gist.id)
-                $('#title').val(data.title)
-                $('#url').val(data.url)
-                $('#description').val(data.description)
-                $('.modal-title').text('Edycja zapiska')
-                $('#modal').modal();
-            });
-            $('#del-' + gist.id).click(function() {
-                if(confirm('Czy chcesz usunąć "' + data.title + '"?')) {
-                    $.ajax({
-                        url: 'https://api.github.com/gists/' + gist.id,
-                        method: "DELETE",
-                        crossDomain: true,
-                        cache: false,
-                        beforeSend: function (xhr) {
-                            xhr.setRequestHeader("Accept", "application/vnd.github.v3+json")
-                            xhr.setRequestHeader("Authorization", "Token " + token)
-                            xhr.setRequestHeader("X-GitHub-OTP", "two-factor-code")
-                        },
-                        success: function (data) {
-                            window.location = start_url;
-                        },
-                        error: function (jqXHR, status, error) {
-                            alert($.parseJSON(jqXHR.responseText).message)
-                        }
-                    });
-                }
-            });
-        }
-        if (data.description != null && data.description !== '')
-            item.after('<div class="col-sm-12 col-md-6 col-lg-8">' + data.description + '</div>');
-        else
-            item.after('<div class="col-sm-12 col-md-6 col-lg-8"><i>Proponowana zakładka</i></div>');
-    }
-}
-function saveGist() {
-    var gistId = $('#gist-id').val();
-    var page = { 
-        type: 'jkpluta.bookmark', 
-        title: $('#title').val(), 
-        url: $('#url').val(),
-        description: $('#description').val()
-    };
-    var data = {
-        "description": "Jan K. Pluta",
-        "public": true,
-        "files": {
-            "bookmark.json": {
-                "content": JSON.stringify(page)
-            }
-        }
-    };
-    var token = localStorage.getItem('token');
-    if (gistId !== '') {
-        $.ajax({
-            url: 'https://api.github.com/gists/' + gistId,
-            method: "PATCH",
-            dataType: "json",
-            crossDomain: true,
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(data),
-            cache: false,
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("Accept", "application/vnd.github.v3+json")
-                xhr.setRequestHeader("Authorization", "Token " + token)
-                xhr.setRequestHeader("X-GitHub-OTP", "two-factor-code")
-            },
-            success: function (data) {
-                window.location = start_url;
-            },
-            error: function (jqXHR, status, error) {
-                alert($.parseJSON(jqXHR.responseText).message)
-            }
-        });
-    } else {
-        $.ajax({
-            url: 'https://api.github.com/gists',
-            method: "POST",
-            dataType: "json",
-            crossDomain: true,
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(data),
-            cache: false,
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("Accept", "application/vnd.github.v3+json")
-                xhr.setRequestHeader("Authorization", "Token " + token)
-                xhr.setRequestHeader("X-GitHub-OTP", "two-factor-code")
-            },
-            success: function (data) {
-                window.location = start_url;
-            },
-            error: function (jqXHR, status, error) {
-                alert($.parseJSON(jqXHR.responseText).message)
-            }
-        });
-    }
 }
 function xor(source1, source2) {
     while (source2.length < source1.length) source2 = source2 + source2;
@@ -287,19 +144,9 @@ function startLogin(href) {
     $('#token').val(localStorage.getItem('token'));
 }
 function startMain(href) {
-  // start('#info', '#info', '/info.html', updateMainInfo);
   start('#icns', '#icns', '/icons.html', updateMainIcons);
   start('#bks', '#bke', '/bookmarks.html', updateMainBookmarks);
-  // startJson('#gists', '#gsts', 'https://api.github.com/users/jkpluta/gists', updateMainGists);
   $('#google').focus();
-  /*
-  $('#save').click(function() {
-    saveGist();
-  });
-  $('#jkp').click(function() {
-    startLogin();
-  })
-  */
 }
 $(document).ready(function() {
   startMain();
